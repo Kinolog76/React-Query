a52f3c4ad15b9b37a44e8245650e6c265c6c62f2
Kinolog76 <vladpasha12@gmail.com>
2024-08-17 14:39:34 +0300

    Add DELETE method, and event page

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,8 +1,33 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useParams, useNavigate } from "react-router-dom";
 
-import Header from '../Header.jsx';
+import Header from "../Header.jsx";
+import { fetchEvent, deleteEvent, queryClient } from "../../utils/http.js";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import LoadingIndicator from "./../UI/LoadingIndicator";
+import ErrorBlock from "./../UI/ErrorBlock";
 
 export default function EventDetails() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["event-post", { search: id }],
+    queryFn: ({ signal }) => fetchEvent({ signal, id }),
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["events"], refetchType: "none" });
+      navigate("/events");
+    },
+  });
+
+  function handleDelete(id) {
+    console.log(mutate);
+    mutate({ id });
+  }
+
   return (
     <>
       <Outlet />
@@ -11,25 +36,35 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="http://localhost:3000/" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
-            </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
-          </div>
+      {isLoading && (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <LoadingIndicator />
         </div>
-      </article>
+      )}
+      {isError && <ErrorBlock title="An error occurred" message={error.info?.message} />}
+      {data && (
+        <article id="event-details">
+          <header>
+            <h1>{data.title}</h1>
+            <nav>
+              <button onClick={() => handleDelete(id)}>Delete</button>
+              <Link to="edit">Edit</Link>
+            </nav>
+          </header>
+          <div id="event-details-content">
+            <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
+            <div id="event-details-info">
+              <div>
+                <p id="event-details-location">{data.location}</p>
+                <time dateTime={`Todo-DateT$Todo-Time`}>
+                  {data.date} / {data.time}
+                </time>
+              </div>
+              <p id="event-details-description">{data.description}</p>
+            </div>
+          </div>
+        </article>
+      )}
     </>
   );
 }
